6d9bf96c50e1a016fe87f1df5db4913c39a459ef
Emir Özbir <emirozbir@hotmail.com>
2018-03-22 03:53:07 +0200

    Params added listen.py and elastic base classes written

--- venv/listen.py
+++ venv/listen.py
@@ -1,18 +1,27 @@
-import select
+import select, argparse,json
 import psycopg2
 import psycopg2.extensions
+
+parser = argparse.ArgumentParser()
+parser.add_argument("channel_name")
+args = parser.parse_args()
+channel_name = args.channel_name
+
+
+
 conn = psycopg2.connect(dbname="testopesto", user="emirozbir", password=" ")
 conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
-
 curs = conn.cursor()
 curs.execute("LISTEN pricesinserted;")
 
-print "Waiting for notifications on channel 'test'"
+
+print "Waiting for notification {}".format(channel_name)
 while 1:
     if select.select([conn],[],[],5) == ([],[],[]):
-        print "Timeout"
+        pass
     else:
         conn.poll()
         while conn.notifies:
             notify = conn.notifies.pop(0)
-            print "Got NOTIFY:", notify.pid, notify.channel, notify.payload
+            json_data = json.loads(notify.payload)
+            print(json_data['salary'])
