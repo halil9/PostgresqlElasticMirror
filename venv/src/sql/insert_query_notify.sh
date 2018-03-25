77f4a7aec3618bfd8a4ef603f3f590682c60c77a
Emir Özbir <emirozbir@hotmail.com>
2018-03-26 02:02:15 +0300

    SQL shell scripts added

--- venv/src/sql/insert_query_notify.sh
+++ venv/src/sql/insert_query_notify.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 TABLE_NAME=$1
 DB_NAME=$2
 FILE_NAME="$2_$1_INSERT.sql"
@@ -7,3 +8,6 @@ sed -i  -e "s/notify_CHANNEL_NAME/notify_$DB_NAME/g" test.sql
 cat test.sql-e > "$FILE_NAME"
 sed -i -e "s/TABLE_NAME/$TABLE_NAME/g" "$FILE_NAME" 
 rm *.sql-e
+
+
+psql -d bugun_ne_yapsam_api_development -c "$(cat $FILE_NAME)"
